8c84725d16c10e1eb8d48382407e6c134a859af8
FraOliv <olivotti.francesco@gmail.com>
2021-02-17 16:04:54 +0100

    added  blog-controller

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('blog', function () {
-    return view('blog');
-});
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('/', 'PageController@index')
+-> name('home');
+Route::get('about', 'PageController@about')
+-> name('about');
 
+Route::get('blog', 'PageController@blog')
+-> name('blog');
