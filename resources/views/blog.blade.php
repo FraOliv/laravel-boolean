9e8a9434da7d4ad0dad83a8fe308cce5765b184c
FraOliv <olivotti.francesco@gmail.com>
2021-02-17 21:45:26 +0100

    1.0

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,12 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-       <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
     <style>
 
@@ -16,15 +15,27 @@
 
     </style>
 </head>
+
 <body>
-    <h1>BLOG</h1>
+    <h1>Blog</h1>
     @foreach ($posts as $item)
-    
-    <h2> {{ $item->title   }} </h2>
-    <h3> {{ $item->body   }} </h3>        
-    @endforeach
-    
+
+
+
+
+
+<div class="blog-wrapper">
+	<div class="blog-card">
+		<div class="card-img"><img src="https://images.unsplash.com/photo-1518235506717-e1ed3306a89b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80">
+			<h1>Olivotti's Blog</h1>
+		</div>
+		<div class="card-details"><span><strong>{{ $item->title  }} </strong></span><span></div>
+		<div class="card-text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si verbum sequimur, primum longius verbum praepositum quam bonum.</p></div>
+		<div class="read-more">Read More</div>
+	</div>
+
+</div>
+@endforeach
 </body>
+
 </html>
-
-
